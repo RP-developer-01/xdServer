ce00a66926e207f4d5c58306534b400992c21076
RP-Developer <98476877+RP-developer-01@users.noreply.github.com>
2023-05-14 19:18:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-// Smooth scrolling to section
+//scroll
 document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
+});
 
 
-  let windowHeight = window.innerHeight;
+//bee
+let windowHeight = window.innerHeight;
 const adjust = () => {
   windowHeight = window.innerHeight;
   if (windowHeight < 800) {
@@ -25,18 +26,23 @@ window.addEventListener("resize", adjust);
 adjust();
 
 
-// Toggle sidebar menu when the menu button is clicked
+//mobile menu
 const menuToggle = document.querySelector('.menu-toggle');
 const sidebar = document.querySelector('.sidebar');
+const menuLinks = document.querySelectorAll('.mobile-link');
 
 menuToggle.addEventListener('click', () => {
   document.body.classList.toggle('menu-open');
 });
 
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('menu-open');
+});
+});
 
 
-/*header*/
-
+//header class scroll
 document.addEventListener("scroll", function() {
   var header = document.querySelector(".header");
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -46,4 +52,25 @@ document.addEventListener("scroll", function() {
   } else {
     header.classList.remove("header-scroll"); /* удаляем класс, если страница находится вверху */
   }
+});
+
+
+//copy button .copyBtn
+const copyBtn = document.getElementById('copyBtn');
+const ipAddress = document.getElementById('ipAddress');
+
+copyBtn.addEventListener('click', () => {
+  const tempInput = document.createElement('input');
+  tempInput.value = ipAddress.innerText;
+  document.body.appendChild(tempInput);
+  
+  tempInput.select();
+  document.execCommand('copy');
+  
+  document.body.removeChild(tempInput);
+  
+  copyBtn.innerText = 'Скопировано!';
+  setTimeout(() => {
+    copyBtn.innerText = 'IP сервера: ' + ipAddress.innerText;
+  }, 1000);
 });
